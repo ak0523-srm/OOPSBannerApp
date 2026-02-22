f7d7af66b46e5051efc48aa97292a58a88edcef6
ANUJ KUMAR <ak0523@srmist.edu.in>
2026-02-22 21:59:27 +0530

    Added UC3 program

---
+++ sumofTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class sumofTwoNumbers{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int b=sc.nextInt();
+        System.out.print("Sum of two numbers: ");
+        System.out.print(a+b);                                                                                                                                                           
+                                                      
+    }
+}
---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable{
+    public static void main(String args[]){
+        double x=5.3;
+        int y=7;
+        float z=7.5f;
+        System.out.println(x+y-z);
+        System.out.println(x-y+z);
+        System.out.println(x*y*z);
+        System.out.print(x/y*z);
+    }
+}
